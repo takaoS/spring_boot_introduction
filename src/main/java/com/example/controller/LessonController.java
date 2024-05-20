6248997345ccb9eb07760458515ba3912ea30de4
takaoS <s.takao0710@gmail.com>
2024-05-20 19:19:59 +0900

    Controller 内でテンプレートを表示

--- src/main/java/com/example/controller/LessonController.java
+++ src/main/java/com/example/controller/LessonController.java
@@ -2,31 +2,21 @@ package com.example.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-//このクラスをControllerとして認識させる
 @Controller
-//ControllerにどんなURLでアクセスするのかを設定
-//クラス、メソッドどちらにも指定が可能で、クラスに対して記述することで共通のURLを設定できる
 @RequestMapping("/lesson")
 public class LessonController {
-	// クラスに指定したURL以降のパスを指定
-	// http://localhost:8080/lesson/ にリクエストすると index()が実行される
 	@GetMapping("/")
-	// レスポンスにメソッドの返り値を利用 (ブラウザが画面に出力)
-	// 今回は Hello World! をブラウザが解読し画面に表示する
-	@ResponseBody
+	// HTML をレスポンスにする場合、@ResponseBody は不要
 	public String index() {
-		return "Hello World!";
+		return "index";  // return "テンプレートファイル名";
 	}
 
-	// http://localhost:8080/lesson/test/1 にアクセスした場合、@PathVariable Integer number の実引数は「1」となる
-	@GetMapping("/test/{num}")
+	@GetMapping("/test")
 	@ResponseBody
-	// 引数名は number としたが name = "num" としたことでパスの値を受け取れる
-	public String test(@PathVariable(name = "num") Integer number) {
+	public String test() {
 		return "This is test!";
 	}
 }
