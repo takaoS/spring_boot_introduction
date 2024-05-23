ea4c887882943857685356f962a65f1082936fff
takaoS <s.takao0710@gmail.com>
2024-05-23 17:50:26 +0900

    GETパラメータの利用
    GETメソッドは、主にサーバから情報を取得したいときに用いる。アドレスバーからの URL入力やリンクからリクエストは、通常すべてGETメソッド

--- src/main/java/com/example/controller/LessonController.java
+++ src/main/java/com/example/controller/LessonController.java
@@ -4,22 +4,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/lesson")
 public class LessonController {
 	@GetMapping("/")
-	// HTML をレスポンスにする場合、@ResponseBody は不要
 	public String index() {
-		return "index";  // return "テンプレートファイル名";
+		return "index";
 	}
 
+	//「?」以降の文字列をクエリ文字列 (クエリストリング、URLパラメータ、リクエストパラメータ) と呼ぶ
+	//「パラメータ名=値」の形で表され、「&」で区切ることで複数のパラメータを渡すことができる
+	// 例：http://localhost:8080/lesson/test?name=太郎&bloodType=AB
+	// GETパラメータは様々なところに記録として残るため、パスワードなど第三者に知られてはいけない情報を記述しないのが基本
 	@GetMapping("/test")
-	// Model型の仮引数を持つ
-	public String test(Model model) {
+	// public String メソッド名(@RequestParam(パラメータ名) 型 仮引数名)
+	// パラメータ名と仮引数名が同じ場合、@RequestParam String bloodType のようにパラメータ名の指定を省略できる
+	public String test(Model model, @RequestParam("name") String nm, @RequestParam String bloodType) {
 		String text = "This is test!";
-		// msg という属性名で "This is test!" を保持
 		model.addAttribute("msg", text);
+		
+		model.addAttribute("name", nm);
+		model.addAttribute("bloodType", bloodType);
+		
 		return "index";
 	}
 }
