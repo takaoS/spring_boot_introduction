430a9b3dca454dd7488ad50566740bd7426cef48
takaoS <s.takao0710@gmail.com>
2024-06-04 19:47:32 +0900

    データの全件取得

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -1,9 +1,14 @@
 package com.example.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.entity.Employee;
 import com.example.service.EmployeeService;
 
 @Controller
@@ -14,5 +19,12 @@ public class EmployeeController {
 	@Autowired
 	public EmployeeController(EmployeeService employeeService) {
 		this.employeeService = employeeService;
+	}
+	
+	@GetMapping("/list")
+	public String showList(Model model) {
+		List<Employee> employees = this.employeeService.findAllEmployee();
+		model.addAttribute("employees", employees);
+		return "employee/list";
 	}
 }
--- src/main/java/com/example/service/EmployeeService.java
+++ src/main/java/com/example/service/EmployeeService.java
@@ -2,9 +2,12 @@ package com.example.service;
 
 // Service ... ビジネスロジックを担うクラス。Repository にデータ取得を依頼し、受け取ったデータの加工などを行う
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.entity.Employee;
 import com.example.repository.EmployeeRepository;
 
 // Serviceクラスであることを明示
@@ -15,5 +18,12 @@ public class EmployeeService {
 	@Autowired
 	public EmployeeService(EmployeeRepository employeeRepository) {
 		this.employeeRepository = employeeRepository;
+	}
+	
+	// 全件取得処理
+	public List<Employee> findAllEmployee() {
+		// employeesテーブルのデータを employeeオブジェクトに入れて取得
+		// 無条件なデータ検索となっているため、Entityクラスの複数取得を想定し、返り値の型は List<Entityクラス> となっている
+		return this.employeeRepository.findAll();
 	}
 }
